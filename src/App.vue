c9448b7b1d0a24fb90dd128a8492321b7bee85e8
Jung Ju Hee <jieboong82@daum.net>
2020-08-31 23:54:32 +0900

    hello 창 css 수정

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@ import TodoHello from "./components/TodoHello";
 import TodoList from "./components/TodoList";
 import TodoTitle from "./components/TodoTitle";
 import getDate from './assets/getDate.js';
+import axios from 'axios';
 
 export default {
   name: "App",
@@ -76,6 +77,8 @@ export default {
           time: getDate().time,
           completed:false
         };
+        // var url = 'https://jsonplaceholder.typicode.com/todos/1';
+        // axios.post(url, value);
 
       localStorage.setItem(todoItem, JSON.stringify(value));
       this.todoItems.push(value);
